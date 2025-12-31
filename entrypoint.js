fb164ad3580937944c55744a01b03da2514905e2
Tryhard <your-email@example.com>
2025-12-31 19:48:46 +0530

    changes

--- entrypoint.js
+++ entrypoint.js
@@ -1,17 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const settings = document.querySelector(".settings");
-    const settingsMenu = document.querySelector(".settingsmenu");
-    const closeBtn = document.querySelector(".close");
-    const value1 = document.querySelector(".value1");
-
-    // Show/hide settings menu
-    settings.addEventListener("click", () => {
-        settingsMenu.classList.toggle("none");
-    });
-
-    closeBtn.addEventListener("click", () => {
-        settingsMenu.classList.toggle("none");
-    });
 
     // Load current setting from localStorage
     let cordsdisplaysetting = localStorage.getItem("cordsdisplaysetting") || "Enable";
